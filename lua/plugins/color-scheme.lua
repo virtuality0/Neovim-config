db96d658ec970b8636fb6a94d1e2a253ede43ca7
virtuality0 <ayushjuyal246@gmail.com>
2026-08-24 15:49:27 +0530

    color scheme change

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,37 +1,85 @@
 return {
-	-- {
-	--   "catppuccin/nvim",
-	--   lazy = false,
-	--   name = "catppuccin",
-	--   priority = 1000,
-	--   config = function()
-	--     vim.cmd.colorscheme "catppuccin-mocha"
-	--   end
-	-- }
-	-- {
-	--   "ellisonleao/gruvbox.nvim",
-	--   lazy = false,
-	--   priority = 1000,
-	--   config = function()
-	--     vim.o.background = "dark" -- or "light"
-	--     vim.cmd.colorscheme("gruvbox")
-	--   end,
-	-- }
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- Optional configuration
-			require("kanagawa").setup({
-				compile = false,
-				transparent = false,
-				dimInactive = false,
-				terminalColors = true,
-				theme = "wave", -- "wave", "dragon", or "lotus"
-			})
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false,
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme "catppuccin-mocha"
+  --   end
+  -- }
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.o.background = "dark" -- or "light"
+  --     vim.cmd.colorscheme("gruvbox")
+  --   end,
+  -- }
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optional configuration
+      require("kanagawa").setup({
+        compile = false,
+        transparent = false,
+        dimInactive = false,
+        terminalColors = true,
+        theme = "dragon"
+      })
 
-			vim.cmd.colorscheme("kanagawa")
-		end,
-	},
+      vim.cmd.colorscheme("kanagawa")
+    end,
+  },
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false,
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme "catppuccin-mocha"
+  --   end
+  -- }
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.o.background = "dark" -- or "light"
+  --     vim.cmd.colorscheme("gruvbox")
+  --   end,
+  -- }
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("kanagawa").setup({
+  --       theme = "dragon", -- "wave", "dragon", or "lotus"
+  --
+  --       compile = true,
+  --       transparent = false,
+  --       terminalColors = true,
+  --
+  --       commentStyle = { italic = true },
+  --       keywordStyle = { italic = true },
+  --       statementStyle = { bold = true },
+  --
+  --       colors = {
+  --         theme = {
+  --           all = {
+  --             ui = {
+  --               bg_gutter = "none", -- cleaner sign column
+  --             },
+  --           },
+  --         },
+  --       },
+  --     })
+  --
+  --     vim.cmd.colorscheme("kanagawa-dragon")
+  --   end,
+  -- },
 }
